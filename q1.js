05221b5ceb0301d517c676202241c9e5f269e799
Amaan-elahi <165452716+Amaan-elahi@users.noreply.github.com>
2024-05-17 23:52:31 +0530

    Add files via upload

---
+++ q1.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+import { getDatabase, ref, onValue, update, push, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-database.js";
+
+const firebaseConfig = {
+    databaseURL: "https://dcrypt-a3adb-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const txt = document.getElementById('input');
+    const button = document.getElementById('btn');
+    if (button) {
+        button.addEventListener('click', () => {
+            const ans = txt.value.trim();
+            console.log(ans);
+            const dbRef = ref(getDatabase())
+            
+            get(child(dbRef, 'Questions/Q1')).then((snapshot) => {
+                if (snapshot.exists()){
+                    console.log(snapshot.val())
+                    const ansC = snapshot.val().trim()
+                    if ( ansC === ans) {
+                        console.log('correct')
+                        window.open('https://forms.gle/jZk9YWMk8mtcEgbeA', "_blank_")
+                        window.location.href = 'hunt_q2.html'
+                    } else{
+                        console.log('Wrong')
+                    }
+                } else {
+                    console.log('No data')
+                }
+            }).catch((error) => {
+                console.log('Error')
+            })  
+        });
+    } else {
+        console.log('Button element not found');
+    }
+});
